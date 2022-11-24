d443970e30b956e8bd3524c86d1a85eacd35063a
Minvydas <mimciks@gmail.com>
2022-11-24 19:12:02 +0200

    retarded commit for my lecture

--- frontend/apps/frontend/src/app/app.component.ts
+++ frontend/apps/frontend/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  myFunc() {
+    let clickCount = 0;
+
+    console.log(clickCount);
+  }
+
+  myFuncB(abc) {
+    if (abc === 1) {
+      console.log(abc);
+    } else {
+    }
+  }
 }
