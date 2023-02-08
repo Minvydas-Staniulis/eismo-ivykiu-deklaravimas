18e90ce4ce954b83516b8cc3f5702ba17b214676
Minvydas-Staniulis <mimciks@gmail.com>
2023-02-08 20:08:50 +0200

    nothing

--- backend/deklaracijos/users/serializers.py
+++ backend/deklaracijos/users/serializers.py
@@ -5,4 +5,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name')
-        read_only_fields = ('username', )
+        #read_only_fields = ('username', )
--- backend/deklaracijos/users/views.py
+++ backend/deklaracijos/users/views.py
@@ -8,7 +8,6 @@ from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
